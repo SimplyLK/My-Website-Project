8eee9daf8b146110002054d080a92644b7138f36
Laura <laura.kan@hotmail.com>
2021-03-21 19:03:55 +1000

    java script updates

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const h2 = document.createElement('h2');
-h2.textContent = "";
-document.querySelector('body').appendChild(h2);
+ function learningInProcess() {
+    let learning = prompt("Laura is currently learning, please be patient? Yes or No?");
+    learning = learning.toLowerCase();
+    if (learning === "yes") {
+         alert("That is great! Thank you for being so patient 😃")
+    } else {
+         alert("All good things come to those who wait 😉");
+    }
+    }
+
+    let learningButton = document.querySelector("button");
+
+    learningButton.addEventListener("click", learningInProcess);
